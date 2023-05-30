8acd8d480c1c69f5d7035a658b3af9bcedb729d3
jakoboto <kurtjoshuavivas@gmail.com>
2023-05-30 12:20:32 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDQDgHWkEufA9fTBIvCI3ML5ZzqJ1QWevA",
+  authDomain: "brigada-daet.firebaseapp.com",
+  projectId: "brigada-daet",
+  storageBucket: "brigada-daet.appspot.com",
+  messagingSenderId: "722071137484",
+  appId: "1:722071137484:web:09a547bf2e76cc391a2669",
+  measurementId: "G-MJQVLMFWXF"
+};
